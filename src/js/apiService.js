952c036dc6d706cdec785e9879283bb1d7a028c1
Vladimyr Kulagin <vladimyrkulagin@gmail.com>
2021-10-02 01:12:53 +0300

    Добавляет функциональную кнопку Load more и загружает новые картинки при повторном сабмите, обнуляет страницу при новом поиске.

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,5 +1,5 @@
-export default async function getFetch(search) {
-  const page = '1';
+export default async function getFetch(search, page) {
+  // const page = '1';
   const perPage = '12';
   const promise = await fetch(
     `https://pixabay.com/api/?key=23604393-9751c4fdb7943747aa1d7afad&q=${search}&page=${page}&per_page=${perPage}&image_type=photo`,
@@ -8,5 +8,6 @@ export default async function getFetch(search) {
     throw new Error(promise.status);
   }
   const data = await promise.json();
+  console.log(promise);
   return data.hits;
 }
--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,84 @@ import template from '../templates/main.hbs';
 
 const list = document.querySelector('.gallery');
 const form = document.getElementById('search-form');
+const btnMore = document.createElement('button');
+btnMore.textContent = 'Load more';
+const modal = document.querySelector('.js-lightbox');
+const modalImg = document.querySelector('.lightbox__image');
+const modalBtnClose = document.querySelector('[data-action="close-lightbox"]');
+const modalOverlay = document.querySelector('.lightbox__overlay');
+
+let page = 1;
+let query = '';
+let isStartedBtn = false;
+let isStartedList = false;
+let firstNewElem = 0;
+let data;
 
 form.addEventListener('submit', async e => {
-  list.innerHTML = '';
   e.preventDefault();
-  list.insertAdjacentHTML('beforeend', template(await getFetch(e.target.elements.query.value)));
 
-  form.reset();
+  list.innerHTML = '';
+
+  firstNewElem = 0;
+
+  if (query !== '') {
+    if (query === e.target.elements.query.value) {
+      page = page + 1;
+    } else {
+      page = 1;
+    }
+  }
+
+  data = await getFetch(e.target.elements.query.value, page);
+  list.insertAdjacentHTML('beforeend', template(data));
+
+  list.appendChild(btnMore);
+
+  query = e.target.elements.query.value;
+
+  if (!isStartedBtn) {
+    isStartedBtn = true;
+    btnMore.addEventListener('click', async () => {
+      page = page + 1;
+      firstNewElem = firstNewElem + 12;
+      data = await getFetch(e.target.elements.query.value, page);
+
+      list.insertAdjacentHTML('beforeend', template(data));
+      const elems = document.querySelectorAll('.photo-card');
+      elems[firstNewElem].scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+
+      list.appendChild(btnMore);
+    });
+  }
+
+  if (!isStartedList) {
+    isStartedList = true;
+
+    list.addEventListener('click', async e => {
+      if (e.target.nodeName === 'IMG') {
+        const targetSrc = e.target.src;
+        const targetData = data.find(({ webformatURL }) => webformatURL === targetSrc);
+        const LargeImgSrc = targetData.largeImageURL;
+
+        modal.classList.add('is-open');
+        modalImg.setAttribute('src', LargeImgSrc);
+        modalBtnClose.addEventListener('click', closeModal);
+        modalOverlay.addEventListener('click', closeModal);
+        window.addEventListener('keydown', e => {
+          if (e.key === 'Escape') {
+            closeModal();
+          }
+        });
+      }
+    });
+  }
 });
+
+function closeModal() {
+  modal.classList.remove('is-open');
+  modalImg.setAttribute('src', '');
+}
